609be781e7acff55d141b8a34a9a285e492de222
Max <silverhammermba@gmail.com>
2016-11-22 14:12:07 -0500

    simplify run to use prebuilt agents confs
    realistically we won't be changing confs much

--- run.sh
+++ run.sh
@@ -4,76 +4,39 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-mkagentconf () {
-	UTEAM=${0^^}
-	LTEAM=${0,,}
-	AGENT=$1
-
-	mkdir team$UTEAM
-	pushd team$UTEAM
-
-	cat >eismassimconfig.xml <<TMP
-<?xml version="1.0" encoding="UTF-8"?>
-<interfaceConfig scenario="mars2013" host="localhost" port="12300" scheduling="yes" times="no" notifications="no" queued="no" statisticsFile="no" statisticsShell="no" submitStatistic="no">
-<entities>
-TMP
-	for i in {1..10}; do
-		cat >>eismassimconfig.xml "<entity name=\"connection${UTEAM}$i\" username=\"${LTEAM}$i\" password=\"1\" iilang=\"yes\" xml=\"yes\"/>"
-	done
-	cat >>eismassimconfig.xml <<TMP
-  </entities>
-</interfaceConfig>
-TMP
-
-	cat >javaagentsconfig.xml <<TMP
-<?xml version="1.0" encoding="UTF-8"?>
-<javaAgentsConfig>
-  <agents>
-TMP
-	for i in {1..10}; do
-		cat >>javaagentsconfig.xml "<agent name=\"agent${UTEAM}$i\" entity=\"connection${UTEAM}$i\" class=\"$AGENT\" team=\"$UTEAM\"/>"
-	done
-	cat >>javaagentsconfig.xml <<TMP
-  </agents>
-</javaAgentsConfig>
-TMP
-	popd
-}
-
 # main project directory
 basedir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # JARs
 server_package=$basedir/massim/target/agentcontest-2013-1.4.jar
 agent_package=$basedir/javaagents/target/javaagents-2.1.jar
 
+default_server_conf=2013-cse431comp.xml
+
 if [ $# -lt 3 ] || [ $# -gt 4 ]; then
-	echo "usage: $0 OUTDIR AGENTA AGENTB [SERVERCONF]" >&2
+	cat >&2 <<USAGE
+usage: $0 OUTDIR ACONF BCONF [SERVERCONF]
+
+	Run the teams in directories ACONF and BCONF using server config in
+	SERVERCONF. Store all output in the directory OUTDIR.
+
+	SERVERCONF defaults to $default_server_conf.
+USAGE
 	exit 1
 fi
 
 output=$1
 agenta=$2
 agentb=$3
-conf=${4:-$basedir/massim/scripts/conf/2013-cse431comp.xml}
-
-if [ -f $output ]; then
-	echo "$output already exists in system" >&2
-	exit 1
-fi
+server_conf=${4:-$basedir/massim/scripts/conf/$default_server_conf}
 
 mkdir $output
-cd $output
+pushd $output
+mkdir backup
 
-mkagentconf "A" $agenta
-mkagentconf "B" $agentb
+(popd; cd $agenta; java -ea -jar $agent_package >$basedir/$output/teama.log 2>$basedir/$output/teama_err.log) &
+(popd; cd $agentb; java -ea -jar $agent_package >$basedir/$output/teamb.log 2>$basedir/$output/teamb_err.log) &
+(java -Xss20000k -cp $server_package massim.competition2013.monitor.GraphMonitor -rmihost localhost -rmiport 1099 -savexmls >/dev/null 2>/dev/null) &
 
-(cd teamA; java -ea -jar $agent_package) &
-(cd teamB; java -ea -jar $agent_package) &
-
-java -ea -Dcom.sun.management.jmxremote -Xss2000k -Xmx600M  -DentityExpansionLimit=1000000 -DelementAttributeLimit=1000000 -Djava.rmi.server.hostname=$(hostname -f)\
+java -ea -Dcom.sun.management.jmxremote -Xss2000k -Xmx600M -DentityExpansionLimit=1000000 -DelementAttributeLimit=1000000 -Djava.rmi.server.hostname=$(hostname -f)\
 	-jar $server_package\
-	--conf $conf
-
-# TODO create dir for each agent containing eismassconfig.xml and javaagentsconfig.xml
-# first just has connections/usernames/passwords
-# second has name/connection/javaclass/team
+	--conf $server_conf
