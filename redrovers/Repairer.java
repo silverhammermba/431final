14f6c6825f0b74f6fde23bba38b27a2b3441f68d
Yi Hu <yih415@lehigh.edu>
2016-12-06 10:56:41 -0500

    repairer

---
+++ redrovers/Repairer.java
@@ -0,0 +1,50 @@
+package redrovers;
+import eis.iilang.Action;
+import eis.iilang.Identifier;
+import eis.iilang.Parameter;
+import eis.iilang.Percept;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import apltk.interpreter.data.Belief;
+import apltk.interpreter.data.LogicBelief;
+import apltk.interpreter.data.Message;
+import massim.javaagents.Agent;
+import massim.javaagents.agents.MarsUtil;
+
+public class Explorer extends RedAgent
+{
+	public Explorer(String name, String team){
+		super(name,team);
+	}
+	
+	Action think(){
+		
+		if(!role.equals("Explorer")){
+			System.err.println("wrong class for agent!");
+			return MarsUtil.skipAction();	
+		}
+		
+		
+		List<String> nodes;
+		for(int i = 0; i < visRange; ++i){
+			nodes = graph.nodesAtRange(position,visRange);
+			for(String id:nodes){
+				if(graph.nodeValue(id) == null){
+					if(energy < 1 + i){
+						return MarsUtil.rechargeAction();
+					}
+					return MarsUtil.probeAction(id);
+				}
+			}
+		}
+		
+		return MarsUtil.skipAction();
+		
+	}
+}
