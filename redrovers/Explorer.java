3c5b4deb7f4f6ae3d86dd127349affc6eac1289c
Yi Hu <yih415@lehigh.edu>
2016-12-08 16:25:18 -0500

    explorer

--- redrovers/Explorer.java
+++ redrovers/Explorer.java
@@ -1,4 +1,6 @@
 package redrovers;
+
+import java.util.Random;
 import eis.iilang.Action;
 import eis.iilang.Identifier;
 import eis.iilang.Parameter;
@@ -26,6 +28,9 @@ import massim.javaagents.agents.MarsUtil;
  */
 public class Explorer extends RedAgent
 {
+	
+	private LinkedList<String> pathList; //store current path node list
+	
 	public Explorer(String name, String team)
 	{
 		super(name,team);
@@ -34,8 +39,8 @@ public class Explorer extends RedAgent
 	Action think()
 	{
 		
-		//probe
 		
+		//probe	
 		if (!role.equals("Explorer"))
 		{
 			System.err.println("wrong class for agent!");
@@ -43,7 +48,7 @@ public class Explorer extends RedAgent
 		}
 		
 
-		
+		/*
 		List<String> nodes;
 		for (int i = 0; i <= visRange; ++i)
 		{
@@ -63,38 +68,54 @@ public class Explorer extends RedAgent
 		}
 		
 		
-		
-		//inspect
-		/*
-		if (!role.equals("Inspector"))
-		{
-			System.err.println("wrong class for agent!");
-			return MarsUtil.skipAction();
-		}
-		
-
-		
-		List<String> nodes;
-		for (int i = 0; i < visRange; ++i)
-		{
-			nodes = graph.nodesAtRange(position,visRange);
-			for (String id : nodes)
-			{
-				if (graph.nodeValue(id) == null)
-				{
-					if (energy < 1 + i)
-						return MarsUtil.rechargeAction();
-
-					return MarsUtil.inspectAction(id);
-				}
-			}
-		}
+		System.err.println("\n\n now go somewhere else\n\n");
 		*/
 		
 		
-
 		
-		return MarsUtil.skipAction();
+		/*
+		//Node start = getNode(sid);
+		LinkedList<String> pathList = graph.explore(position);
+		for(String pos:pathList){
+			System.err.println("\n\nwill go to " + pos + "\n\n");
+			if(energy < maxEnergy/2){
+				return MarsUtil.rechargeAction();
+			}else{
+				return MarsUtil.gotoAction(pos);
+			}		
+			
+		}
+		*/
+		
+		this.pathList = graph.explore(position);
+		
+		if(energy < 1){ 
+			return MarsUtil.rechargeAction();
+		}
+		
+		if(graph.nodeValue(position) == null){ // try to probe it 
+			return MarsUtil.probeAction();
+		
+		}else{  //try to survey
+			List<String> nodes = graph.nodesAtRange(position,1);
+			if(graph.unknownNearby(position)){
+				return MarsUtil.surveyAction();
+			
+			}else{ //try to goto somewhere according to pathList	
+				String pos = pathList.get(0);
+				if(pathList != null){ //if there is node to go 
+					if(energy < graph.edgeWeight(position,pos)){ //energy too low to go
+						return MarsUtil.rechargeAction();
+					}else{
+						pathList.remove(0);
+						return MarsUtil.gotoAction(pos);
+					}	
+				}
+			
+			}
+			
+		}
 
+		return MarsUtil.skipAction();
 	}
 }
